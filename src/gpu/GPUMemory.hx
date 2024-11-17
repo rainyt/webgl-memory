b29501bc9c7995ef2b3317eee0f4a15956e62566
Rainy <939029835@qq.com>
2024-11-17 21:55:46 +0800

    新增updateGPUMemorySize和getBitmapDatas实现

--- src/gpu/GPUMemory.hx
+++ src/gpu/GPUMemory.hx
@@ -19,12 +19,18 @@ class GPUMemory {
 	public static var bitmapDatas:Array<WeakRef<BitmapData>>;
 
 	/**
+	 * GPU内存缓存记录大小
+	 */
+	private static var __gpuMemorySize:Int = 0;
+
+	/**
 	 * 观察位图引用关系
 	 * Observe bitmap reference relationships
 	 * @param bitmapData 
 	 */
 	public static function which(bitmapData:BitmapData):Void {
 		bitmapDatas.push(new WeakRef(bitmapData));
+		updateGPUMemorySize();
 	}
 
 	/**
@@ -33,6 +39,13 @@ class GPUMemory {
 	 * @return Int
 	 */
 	public static function getGPUMemorySize():Int {
+		return __gpuMemorySize;
+	}
+
+	/**
+	 * 更新GPU内存大小
+	 */
+	public static function updateGPUMemorySize():Void {
 		var size:Int = 0;
 		bitmapDatas = bitmapDatas.filter(ref -> #if cpp ref.get() #else ref.deref() #end != null);
 		for (ref in bitmapDatas) {
@@ -41,6 +54,20 @@ class GPUMemory {
 				size += bitmapData.width * bitmapData.height * 4;
 			}
 		}
-		return size;
+		__gpuMemorySize = size;
+	}
+
+	/**
+	 * Get all bitmapData
+	 * @return Array<BitmapData>
+	 */
+	public static function getBitmapDatas():Array<BitmapData> {
+		bitmapDatas = bitmapDatas.filter(ref -> #if cpp ref.get() #else ref.deref() #end != null);
+		var array = [];
+		for (ref in bitmapDatas) {
+			var bitmapData:BitmapData = #if cpp ref.get() #else ref.deref() #end;
+			array.push(bitmapData);
+		}
+		return array;
 	}
 }
